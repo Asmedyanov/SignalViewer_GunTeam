f30827fe2c416b00d647f8c97d4e8273eaa27a9a
Nikita_Asmedianov <asmed1996@gmail.com>
2021-12-03 20:15:30 +0700

    Оформил начальную версию главного меню

--- classes/mainwindow.py
+++ classes/mainwindow.py
@@ -5,5 +5,5 @@ from PyQt5.QtWidgets import QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        #uic.loadUi('ui/mainwindow.ui', self)
+        uic.loadUi('ui/mainwindow.ui', self)
         self.show()
--- main.py
+++ main.py
@@ -6,17 +6,9 @@ from classes.mainwindow import MainWindow
 import sys
 from PyQt5.QtWidgets import QApplication
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
     app.exec()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
