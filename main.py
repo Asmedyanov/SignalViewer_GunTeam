7178fd2976c829cc55c465b22e1907c51db9b872
Nikita_Asmedianov <asmed1996@gmail.com>
2021-12-03 19:29:31 +0700

    Загрузил главное окно

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from classes.mainwindow import MainWindow
+import sys
+from PyQt5.QtWidgets import QApplication
 
 
 def print_hi(name):
@@ -11,6 +14,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    mainwindow = MainWindow()
+    app.exec()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
