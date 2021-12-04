1692c83bac7e6859d934c22b0fda5c6f739f8bdc
Nikita_Asmedianov <asmed1996@gmail.com>
2021-12-04 18:35:55 +0700

    Инициализировал двухуровневый словарь действий

--- classes/mainwindow.py
+++ classes/mainwindow.py
@@ -1,5 +1,5 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QMainWindow, QSizePolicy, QVBoxLayout
+from PyQt5.QtWidgets import QMainWindow, QSizePolicy, QVBoxLayout, QMenu
 from classes.mplwidget import MplWidget
 
 
@@ -7,7 +7,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         uic.loadUi('ui/mainwindow.ui', self)
+        self.initActions()
+        self.initTabs()
 
+        self.show()
+
+    def initTabs(self):
         self.mainTabTextDict = dict()
         for i in range(self.tabWidget.count()):
             text = self.tabWidget.tabText(i)
@@ -26,4 +31,11 @@ class MainWindow(QMainWindow):
                 layout.addWidget(plotter)
                 page.setLayout(layout)
 
-        self.show()
+    def initActions(self):
+        self.mainMenuDict = dict()
+        self.mainActionsDict = dict()
+        for menu in self.menubar.findChildren(QMenu):
+            self.mainMenuDict[menu.title()] = menu
+            self.mainActionsDict[menu.title()] = dict()
+            for act in menu.actions():
+                self.mainActionsDict[menu.title()][act.text()] = act
