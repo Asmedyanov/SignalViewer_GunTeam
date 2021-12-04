bf40a033e1d472a0ffc90d0b977210386d589fc5
Nikita_Asmedianov <asmed1996@gmail.com>
2021-12-04 13:56:29 +0700

    Создал класс виджетов демо графиков

---
+++ classes/mplwidget.py
@@ -0,0 +1,12 @@
+# Matplotlib widget
+from mplcanvas import MplCanvas
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+
+
+class MplWidget(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)  # Inherit from QWidget
+        self.canvas = MplCanvas()  # Create canvas object
+        self.vbl = QVBoxLayout()  # Set box for plotting
+        self.vbl.addWidget(self.canvas)
+        self.setLayout(self.vbl)
