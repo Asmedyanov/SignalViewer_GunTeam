63293b4318ca74d4d0f90bb7d92878188f4e4215
Nikita_Asmedianov <asmed1996@gmail.com>
2022-01-31 00:17:01 +0700

    Продолжаю возиться с симулятором интерферометра

--- InretferometerSimulator.py
+++ InretferometerSimulator.py
@@ -8,7 +8,8 @@ u2 = 1.0
 npoint = 1e5
 dt = 0.1
 t0 = -700.0
-tau0 = 1.0e6 / 2.0e2
+fgen = 3.0e2
+tau0 = 1.0e6 / fgen
 timeline = np.arange(npoint) * dt + t0
 #fipiezo0 = random() * 2.0 * np.pi
 fipiezo0 = 1.5/2
@@ -44,20 +45,20 @@ interfirence = interfirence - mininterf
 maxinterf = interfirence.max()
 faseafter = np.arccos(1.0 - (2.0 * interfirence / maxinterf))
 
-fstart = 18.0e3
+fstart = 16*fgen
 ffinish = 1.0e7
-f_signal = rfft(faseafter)
+f_signal = rfft(interfirence)
 W = fftfreq(f_signal.size, d=dt*1.0e-6)[:int(f_signal.size)]
 cut_f_signal = f_signal.copy()
 fend = ffinish
 #fw = 1428.5714285714287 * 4
-fw = 2.0*1.0e6/sigmaplasma1
+fw = fstart/30
 fwindow = np.exp(-np.power((W - fstart) / fw, 2)) + np.exp(-np.power((W - fend) / fw, 2))
-
+fwindow = fwindow/np.max(fwindow)
 fwindow = np.where(((np.abs(W) >= fstart) & (np.abs(W) <= fend)), 1, fwindow)
-A = 1.0/(fwindow.sum()*dt*1.0e-6)
-fwindow = fwindow*A
+#fwindow = np.where(((np.abs(W) >= fstart) & (np.abs(W) <= fend)), 1, 0)
 cut_signal = irfft(cut_f_signal * fwindow)
+faseafter_cut= np.arccos(1.0 - (2.0 * cut_signal / maxinterf))
 cut_time = timeline[:cut_signal.size]
 
 xaxis = timeline
@@ -66,6 +67,6 @@ fig, axes = plt.subplots(4, sharex=True)
 axes[0].plot(xaxis, fplasma)
 axes[1].plot(xaxis, interfirence0)
 axes[2].plot(xaxis, faseafter)
-axes[3].plot(cut_time, cut_signal)
+axes[3].plot(cut_time, faseafter_cut)
 plt.ylabel('interfirence')
 plt.show()
